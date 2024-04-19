7356d35427826d91661d0eba9e92509a26f28fc2
karen <karenmourice@gmail.com>
2024-04-19 12:03:12 +0200

    modified by karen

--- src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/ChangeReservationController.java
+++ src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/ChangeReservationController.java
@@ -1,19 +1,33 @@
 package org.example.restaurant_management_system_ui.Services.Reservation;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.example.restaurant_management_system_ui.MainApplication;
+
+import java.io.IOException;
 
 public class ChangeReservationController {
-    public ImageView returnImage1;
-    public Text returnText1;
+    public ImageView returnChangeReservation;
+    public Text textChangeReservation;
 
-    public void press(MouseEvent event) {
+    public void pressChangeReservation(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("reservation.fxml"));
+        Scene reservationScene = new Scene(fxmlLoader.load(), 1280, 720);
+        Stage mainStage=(Stage)returnChangeReservation.getScene().getWindow();
+        mainStage.setScene(reservationScene);
     }
 
-    public void show1(MouseEvent event) {
+    public void showChangeReservation(MouseEvent mouseEvent) {
+        textChangeReservation.setText("Return to reservation page");
     }
 
-    public void hide1(MouseEvent event) {
+    public void hideChangeReservation(MouseEvent mouseEvent) {
+        textChangeReservation.setText("");
     }
+
+
 }
