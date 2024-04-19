d1915fa9f9d610367315758dcb671ab4e6eb539b
karen <karenmourice@gmail.com>
2024-04-19 10:54:36 +0200

    modified by karen

--- src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/ReservationController.java
+++ src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/ReservationController.java
@@ -1,19 +1,33 @@
 package org.example.restaurant_management_system_ui.Services.Reservation;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.example.restaurant_management_system_ui.MainApplication;
+
+import java.io.IOException;
 
 public class ReservationController {
-    public ImageView returnImage1;
-    public Text returnText1;
+    public Text reservationText;
+    public ImageView returnMakeReservation;
+    public Button makeReservationButton;
 
-    public void press(MouseEvent event) {
+    public void showMakeReservation(MouseEvent event) {
+        reservationText.setText("Return to main page");
     }
 
-    public void show1(MouseEvent event) {
+    public void hideMakeReservation(MouseEvent event) {
+        reservationText.setText("");
     }
 
-    public void hide1(MouseEvent event) {
+    public void pressMakeReservation(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("start.fxml"));
+        Scene reservationScene = new Scene(fxmlLoader.load(), 1280, 720);
+        Stage mainStage=(Stage)returnMakeReservation.getScene().getWindow();
+        mainStage.setScene(reservationScene);
     }
 }
