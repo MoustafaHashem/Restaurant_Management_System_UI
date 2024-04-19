72384169525e2cf56e087a966742fe73e50c3d88
karen <karenmourice@gmail.com>
2024-04-19 12:03:27 +0200

    modified by karen

--- src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/CancelReservationController.java
+++ src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/CancelReservationController.java
@@ -1,19 +1,33 @@
 package org.example.restaurant_management_system_ui.Services.Reservation;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.example.restaurant_management_system_ui.MainApplication;
+
+import java.io.IOException;
 
 public class CancelReservationController {
-    public ImageView returnImage1;
-    public Text returnText1;
+    public ImageView returnCancelReservation;
+    public Text textCancelReservation;
 
-    public void press(MouseEvent event) {
+    public void pressCancelReservation(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("reservation.fxml"));
+        Scene reservationScene = new Scene(fxmlLoader.load(), 1280, 720);
+        Stage mainStage=(Stage)returnCancelReservation.getScene().getWindow();
+        mainStage.setScene(reservationScene);
     }
 
-    public void show1(MouseEvent event) {
+    public void showCancelReservation(MouseEvent mouseEvent) {
+        textCancelReservation.setText("Return to reservation page");
     }
 
-    public void hide1(MouseEvent event) {
+    public void hideCancelReservation(MouseEvent mouseEvent) {
+        textCancelReservation.setText("");
     }
+
+
 }
