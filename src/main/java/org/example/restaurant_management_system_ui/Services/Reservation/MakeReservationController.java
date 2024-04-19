e2e5cc9849bb4081b7c23312af8108ace861e6d6
karen <karenmourice@gmail.com>
2024-04-19 12:03:47 +0200

    modified by karen

--- src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/MakeReservationController.java
+++ src/main/java/org/example/restaurant_management_system_ui/Services/Reservation/MakeReservationController.java
@@ -1,23 +1,38 @@
 package org.example.restaurant_management_system_ui.Services.Reservation;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.example.restaurant_management_system_ui.MainApplication;
+
+import java.io.IOException;
 
 public class MakeReservationController {
 
     //set error text to (Your Reservation ID is: **) when there is no error
 
 
-    public ImageView returnImage1;
-    public Text returnText1;
+    public ImageView returnMakeReservation;
 
-    public void press(MouseEvent event) {
+    public Text textMakeReservation;
+
+
+    public void pressMakeReservation(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("reservation.fxml"));
+        Scene reservationScene = new Scene(fxmlLoader.load(), 1280, 720);
+        Stage mainStage=(Stage)returnMakeReservation.getScene().getWindow();
+        mainStage.setScene(reservationScene);
     }
 
-    public void show1(MouseEvent event) {
+    public void showMakeReservation(MouseEvent mouseEvent) {
+        textMakeReservation.setText("Return to reservation page");
     }
 
-    public void hide1(MouseEvent event) {
+    public void hideMakeReservation(MouseEvent mouseEvent) {
+        textMakeReservation.setText("");
     }
+
 }
