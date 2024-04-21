d73e895f0ef6145a122d6c4257c567a57300ef28
karen <karenmourice@gmail.com>
2024-04-21 22:34:49 +0200

    modified by karen

--- src/main/java/Services/Reservation.java
+++ src/main/java/Services/Reservation.java
@@ -2,12 +2,13 @@ package Services;
 
 import Human.Customer;
 import Human.Manager;
-import Restaurant.Table;
+import Restaurant.*;
 
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Reservation {
     private  int reservationId;
@@ -117,51 +118,23 @@ public class Reservation {
         reservations.get(i).setDate(d);
         System.out.println("changed");
     }
-    public static void makeReservation(String name , int age,String address ,String phone,LocalDate date, int numberOfPeople){
+    public static int makeReservation(int tableToBeReserved,String name , int age,String address ,String phone,LocalDate date, int numberOfPeople){
         Reservation r;
-        int i;
-        int selectedTable;
-        boolean invalidTable=true;
-
-        }
-        if (!availableTables.isEmpty())
-        {
-            Customer c = new Customer(name,age,address,phone);
-            r = new Reservation(date,numberOfPeople);
+        ArrayList<Table>availableTables=new ArrayList<>();
+        availableTables=Manager.getAvailableTables();
+        if (!availableTables.isEmpty()) {
+            Customer c = new Customer(name, age, address, phone);
+            r = new Reservation(date, numberOfPeople);
             reservations.add(r);
-            for(Table table:availableTables)
-            {
-                System.out.println("Table "+ table.getTableNum());
-            }
-            do {
-                System.out.println("\nWhich table do you want to make your reservation?");
-                selectedTable = in.nextInt();
-                for (Table table:availableTables) {
-                    if (table.getTableNum() == selectedTable) {
-                        invalidTable = false;
-                        break;
-                    }
-                }
-                if(!invalidTable)
-                {
-                    Manager.getTables().get(selectedTable-1).addReservation(r);
-                    c.checkIn();
-                    Manager.getTables().get(selectedTable-1).setCustomer(c);
-                    Manager.getTables().get(selectedTable-1).setIsReserved(true);
-                    System.out.println("Reservation done successfully");
-                    System.out.println("Your Reservation ID is: "+ r.getReservationId());
-                    System.out.println("************************************************************");
-                }
-                else {
-                    System.out.println("Please enter a valid table number from the available tables listed above");
-                }
-            }while(invalidTable);
-
+            Manager.getTables().get(tableToBeReserved - 1).addReservation(r);
+            c.checkIn();
+            Manager.getTables().get(tableToBeReserved - 1).setCustomer(c);
+            Manager.getTables().get(tableToBeReserved - 1).setIsReserved(true);
+            return 1;
         }
         else
         {
-            System.out.println("No available tables for making a reservation at the restaurant at the moment");
-            System.out.println("************************************************************");
+            return -1;
         }
     }
 
