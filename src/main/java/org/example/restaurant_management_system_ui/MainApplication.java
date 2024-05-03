1a98991dc8c70452868b24789cd83585cd152aa6
mikyg <mikygogo9@hotmail.com>
2024-05-04 01:21:22 +0300

    payment fix

--- src/main/java/org/example/restaurant_management_system_ui/MainApplication.java
+++ src/main/java/org/example/restaurant_management_system_ui/MainApplication.java
@@ -34,12 +34,12 @@ public class MainApplication extends Application {
     }
 
     public static void main(String[] args) {
-        //MediaPlayer media=new MediaPlayer(new Media(new File("src/main/resources/Music/French_Accordion_Traditionell_Musette.mp3").toURI().toString()));
-        MediaPlayer media=new MediaPlayer(new Media(new File("src/main/resources/Music/Le+festib+(mp3cut.net)_out.mp3").toURI().toString()));
+        MediaPlayer media=new MediaPlayer(new Media(new File("src/main/resources/Music/French_Accordion_Traditionell_Musette.mp3").toURI().toString()));
+        //MediaPlayer media=new MediaPlayer(new Media(new File("src/main/resources/Music/Le+festib+(mp3cut.net)_out.mp3").toURI().toString()));
         media.setCycleCount(MediaPlayer.INDEFINITE);
         media.setVolume(.1);
         media.play();
-        Manager owner = new Manager("Moustafa  ", 20, "el-obour city", "01097963663", 10000, "high");
+        Manager owner = new Manager("Batman  ", 20, "Gotham City", "01229082038", 5000000, "high");
         owner.addEmployee(owner);
         Staff kareem = new Staff("kareem ", 20, "el-obour city", "01097963663", 1000000, "high");
         owner.addEmployee(kareem);
