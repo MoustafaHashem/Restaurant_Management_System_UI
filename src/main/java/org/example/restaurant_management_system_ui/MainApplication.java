880041dc7103c9860b4118c4924ada3696df611d
Karim <Karimehab102.ke@gmail.com>
2024-05-11 00:12:45 +0300

    Update

--- src/main/java/org/example/restaurant_management_system_ui/MainApplication.java
+++ src/main/java/org/example/restaurant_management_system_ui/MainApplication.java
@@ -10,12 +10,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
